e33a0c28b880f15045d454da3cda16657b5c1562
Fannstan <fredrica.stanley@gmail.com>
2022-06-06 18:34:19 -0400

    adding queries

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/schema.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/schema.sql
@@ -53,3 +53,39 @@ CREATE TABLE titles (
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL, 
+	from_date DATE NOT NULL, 
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+
+SELECT * FROM departments;
+
+SELECT * FROM departments;
+
+DROP TABLE dept_manager CASCADE;
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL, 
+	from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+DROP TABLE titles CASCADE;
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL, 
+	from_date DATE NOT NULL, 
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+	);
+
+select * From dept_emp
+select * From dept_manager
