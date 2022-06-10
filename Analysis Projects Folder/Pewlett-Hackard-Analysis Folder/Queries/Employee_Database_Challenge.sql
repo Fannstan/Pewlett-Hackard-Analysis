94aef38b61c5f8c414f0f0d011385a2502d0e2c4
Fannstan <fredrica.stanley@gmail.com>
2022-06-10 12:16:50 -0400

    updating with Deliverable 2

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_Challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_Challenge.sql
@@ -1,6 +1,6 @@
---Challenge
+--Challenge - MODULE 7 --
 
---Deliverable 1
+--Deliverable 1 --
 
 SELECT e.emp_no,
 	e.first_name,
@@ -38,3 +38,27 @@ GROUP BY ut.title
 ORDER BY COUNT(ut.title) DESC; 
 
 SELECT * FROM retiring_titles; 
+
+-- Deliverable 2 -- 
+SELECT DISTINCT ON (e.emp_no) 
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no=de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no=t.emp_no)
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+DROP TABLE membership_eligibilty;
+DROP TABLE mentorship_eligibilty;
+	
+SELECT * FROM mentorship_eligibility;
+
